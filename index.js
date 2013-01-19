32ee2b874cd1e31cc21103f583d7d4b81ebd8dfc
Max Ogden <max@maxogden.com>
2013-01-19 14:31:22 -0800

    get it to work

---
+++ index.js
@@ -0,0 +1,40 @@
+var binaryXHR = require('binary-xhr')
+var PNGReader = require('./png.js') // it doesnt work with browserify so we vendor it for now
+var worldHeight = 128
+
+function loadImage(url, cb) {
+  binaryXHR(url, function(err, data) {
+    if (err) return cb(err)
+    var reader = new PNGReader(data)
+    reader.parse(function(png){
+      cb(false, png)
+    })
+  })
+}
+
+function getCoordinate(x, y, z) {
+  x = ~~scale(x, -(worldHeight / 2), (worldHeight / 2), 0, this.width)
+  z = ~~scale(z, -(worldHeight / 2), (worldHeight / 2), 0, this.height)
+  var pixel = this.getPixel(x,z)
+  var r = pixel[1]
+  var height = scale(r, 0, 255, 0, worldHeight/5)
+  if (y<1) return 2
+  return y > height ? 0 : 1
+}
+
+function scale( x, fromLow, fromHigh, toLow, toHigh ) {
+  return ( x - fromLow ) * ( toHigh - toLow ) / ( fromHigh - fromLow ) + toLow
+}
+
+module.exports = function(url, height, cb) {
+  if (typeof height === 'function') cb = height
+  if (height) worldHeight = height
+  loadImage(url, function(err, pngData) {
+    window.png = pngData
+    var returnFunc = getCoordinate.bind(pngData)
+    returnFunc.data = pngData
+    cb(err, returnFunc)
+  })
+}
+
+exports.loadImage = loadImage
