803bbe5e14cbcb600a5bed8144ccd1363dd9e41e
Max Ogden <max@maxogden.com>
2013-01-19 15:24:10 -0800

    use stripped down getpixel function for speed

--- index.js
+++ index.js
@@ -12,14 +12,19 @@ function loadImage(url, cb) {
   })
 }
 
+function getPixel(pngReader, x, y) {
+	var i = pngReader.colors * pngReader.bitDepth / 8 * (y * pngReader.width + x);
+	return pngReader.pixels[i+1]
+};
+
 function getCoordinate(x, y, z) {
   x = ~~scale(x, -(worldHeight / 2), (worldHeight / 2), 0, this.width)
+  y = ~~scale(y, -(worldHeight / 2), (worldHeight / 2), 0, worldHeight)
   z = ~~scale(z, -(worldHeight / 2), (worldHeight / 2), 0, this.height)
-  var pixel = this.getPixel(x,z)
-  var r = pixel[1]
-  var height = scale(r, 0, 255, 0, worldHeight/5)
-  if (y<1) return 2
-  return y > height ? 0 : 1
+  var pixel = getPixel(this, x, z)
+  var height = ~~scale(pixel, 0, 255, 0, worldHeight / 5)
+  if (y < 1) return 2
+  return y > height ? 0 : 1 
 }
 
 function scale( x, fromLow, fromHigh, toLow, toHigh ) {
